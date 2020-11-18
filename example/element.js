d524c78dfbff2c20ee576e473909fd7b3ed501cd
oiemets <iemets.git@gmail.com>
2020-11-18 20:26:13 +0200

    upgrade of initial state

---
+++ example/element.js
@@ -0,0 +1,8 @@
+import '../src/x-550136-incident-list';
+
+const el = document.createElement('DIV');
+document.body.appendChild(el);
+
+el.innerHTML = `		
+<x-550136-incident-list></x-550136-incident-list>
+`;
